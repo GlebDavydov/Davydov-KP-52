577321e2aa2358850be08ee1445f2d8f9fda471b
GlebDavydov <davydovkp52@gmail.com>
2016-05-21 01:39:15 +0300

    part 1 done

---
+++ courses/prog_base_2/labs/lab4/server.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "server.h"
+
+struct keyvalue_s{
+    char key[256];
+    char value[256];
+};
+
+struct http_request_s{
+    char method[8];
+    char uri[256];
+    keyvalue_t * form;
+    int formLength;
+};
+
+int checkDate(char *date){
+    if(strlen(date) != 10){
+        return 0;
+    }
+    for(int i = 0; i < 10; i++){
+        if(i == 2 || i == 5){
+            if(date[i] != '.'){
+                return 0;
+            }
+        } else {
+            if(!isdigit(date[i]))
+                return 0;
+        }
+    }
+    return 1;
+}
+
+http_request_t
+http_request_parse(const char * const request) {
+    http_request_t req;
+    req.form = NULL;
+    req.formLength = 0;
+    // get method
+    ptrdiff_t methodLen = strstr(request, " ") - request;  // find first whitespace
+    memcpy(req.method, request, methodLen);
+    req.method[methodLen] = '\0';
+    // get uri
+    const char * uriStartPtr = request + strlen(req.method) + 1;
+    const char * uriEndPtr = strstr(uriStartPtr, " ");  // find second whitespace
+    ptrdiff_t uriLen = uriEndPtr - uriStartPtr;
+    memcpy(req.uri, uriStartPtr, uriLen);
+    req.uri[uriLen] = '\0';
+    // parse form data
+    const char * bodyStartPtr = strstr(request, "\r\n\r\n") + strlen("\r\n\r\n");
+    const char * cur = bodyStartPtr;
+    const char * pairEndPtr = cur;
+    const char * eqPtr = cur;
+    while (strlen(cur) > 0) {
+        pairEndPtr = strchr(cur, '&');
+        if (NULL == pairEndPtr) {
+            pairEndPtr = cur + strlen(cur);
+        }
+        keyvalue_t kv;
+        // get key
+        eqPtr = strchr(cur, '=');
+        ptrdiff_t keyLen = eqPtr - cur;
+        memcpy(kv.key, cur, keyLen);
+        kv.key[keyLen] = '\0';
+        // get value
+        eqPtr++;
+        ptrdiff_t valueLen = pairEndPtr - eqPtr;
+        memcpy(kv.value, eqPtr, valueLen);
+        kv.value[valueLen] = '\0';
+        // insert key-value pair into request form list
+        req.formLength += 1;
+        req.form = realloc(req.form, sizeof(keyvalue_t) * req.formLength);
+        req.form[req.formLength - 1] = kv;
+        cur = pairEndPtr + ((strlen(pairEndPtr) > 0) ? 1 : 0);
+    }
+    return req;
+}
+
+const char *
+http_request_getArg(http_request_t * self, const char * key) {
+    for (int i = 0; i < self->formLength; i++) {
+        if (strcmp(self->form[i].key, key) == 0) {
+            return self->form[i].value;
+        }
+    }
+    return NULL;
+}
+
+const char * keyvalue_toString(keyvalue_t * self) {
+    char * str = malloc(sizeof(char) * (strlen(self->key) + strlen(self->value) + 2));
+    sprintf(str, "%s=%s\0", self->key, self->value);
+    return str;
+}
+
+void http_request_startServer(list_t *list){
+    lib_init();
+    socket_t * serverSocket = socket_new();
+    socket_bind(serverSocket, 5000);
+    socket_listen(serverSocket);
+    while(1){
+        puts("Waiting for connections");
+        socket_t * clientSocket = socket_accept(serverSocket);
+        puts("New client");
+        char buff[BUFFER_LENGTH];
+        int readLength = socket_read(clientSocket, buff, BUFFER_LENGTH);
+        if(readLength == 0){
+            socket_close(clientSocket);
+            socket_free(clientSocket);
+            puts("Skipping empty request");
+            continue;
+        }
+
+        printf("Got Request:\n---------------\n%s\n----------------\n", buff);
+
+        http_request_t req = http_request_parse(buff);
+
+        printf("Method: %s\nURI: %s\n", req.method, req.uri);
+        puts("Data:");
+        for(int i = 0; i < req.formLength; i++){
+            char * kvStr = keyvalue_toString(&req.form[i]);
+            printf("\t%s\n", kvStr);
+            free(kvStr);
+        }
+        http_request_chooseMethod(req, clientSocket, list);
+        socket_close(clientSocket);
+        socket_free(clientSocket);
+    }
+    socket_close(serverSocket);
+    socket_free(serverSocket);
+    lib_free();
+}
+
+void http_request_chooseMethod(http_request_t req, socket_t * clientSocket, list_t *list){
+    if(!strcmp(req.uri, "/"))
+        {
+        char msg[MSG_LENGTH];
+        sprintf(msg, "<message>\n"
+                    "\t<status>ok</status>\n"
+                    "\t<text>HELLO, USER!</text>\n"
+                    "</message>\n");
+        socket_write_string(clientSocket, msg);
+        }
+    /*else if (!strcmp(req.uri, "/teachers"))
+        {
+        char msg[MSG_LENGTH];
+        sprintf(msg, "<message>\n"
+                    "\t<status>error</status>\n"
+                    "\t<text>Not Found</text>\n"
+                    "</message>\n");
+        socket_write_string(clientSocket, msg);
+        }*/
+    else
+        if (strcmp(req.uri, "/teachers") == 0)
+        {
+            if(strcmp(req.method, "GET") == 0)
+                {
+                    char result[10000];
+                    sprintf(result,
+                        "HTTP/1.1 200 OK\n"
+                        "Content-length: %zu\n"
+                        "Content-type: application/xml\n"
+                        "\n"
+                        "%s\0",
+                        strlen(all_teachers_to_message(list)), all_teachers_to_message(list));
+                    socket_write_string(clientSocket, result);
+                }
+            else {
+                char result_msg[MSG_LENGTH];
+                sprintf(result_msg,
+                        "<message>\n"
+                        "\t<status>error</status>\n"
+                        "\t<text>NOT ALLOWED</text>\n"
+                        "</message>\n");
+                socket_write_string(clientSocket, result_msg);
+            }
+        }
+    else if (strcmp(req.uri, "/teachers") > 0)
+        {if(strcmp(req.method, "POST") == 0)
+                {
+                    char result[MSG_LENGTH];
+                    const char * fName = http_request_getArg(&req, "firstName");
+                    const char * lName = http_request_getArg(&req, "lastName");
+                    const char * pensDate = http_request_getArg(&req, "pensionDate");
+                    double rating = atof(http_request_getArg(&req, "rating"));
+                    int  hours = atoi(http_request_getArg(&req, "hours"));
+                    const char * cName = http_request_getArg(&req, "cathedra");
+                    const char * sName = http_request_getArg(&req, "speciality");
+                    const char * g1Name = http_request_getArg(&req, "group1");
+                    const char * g2Name = http_request_getArg(&req, "group2");
+                    if(!fName || !lName || !pensDate ||
+                       !cName || !sName || !g1Name || !g2Name){
+                        sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Empty argument(s)</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                       }
+                    else if(strlen(fName) == 0 || strlen(fName) > WORD_LENGTH || strlen(lName) == 0 || strlen(lName) > WORD_LENGTH){
+                            sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Invalid name</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                    } else if(!checkDate(pensDate)){
+                        sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Invalid pension date</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                    } else if(strlen(cName) == 0 || strlen(cName) > WORD_LENGTH){
+                        sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Invalid cathedra</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                    } else if(rating < 0 || rating > 10.0){
+                        sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Invalid score</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                    } else if(strlen(sName) == 0 || strlen(sName) > WORD_LENGTH){
+                        sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Invalid speciality</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                    } else if(strlen(g1Name) == 0 || strlen(g1Name) > WORD_LENGTH || strlen(g2Name) == 0 || strlen(g2Name) > WORD_LENGTH){
+                        sprintf(result,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Invalid group</text>\n"
+                            "</message>\n");
+                            socket_write_string(clientSocket, result);
+                            return 1;
+                     }else {
+                    int this = list_add(list, new_teacher());
+                    teacher_update(list_get(list, list_find_id(list, this)), fName, lName, pensDate, hours, rating, cName, sName, g1Name, g2Name);
+                       printf("New:\n"
+                           "<teacher>\n"
+                            "\t<id>%i</id>\n"
+                            "\t<firstName>%s</firstName>\n"
+                            "\t<lastName>%s</lastName>\n"
+                            "\t<pensionDate>%s</penstionDate>\n"
+                            "\t<hours>%d</hours>\n"
+                            "\t<rating>%.2f</rating>\n"
+                            "\t<cathedra nameCathedra=\"%s\">\n"
+                            "\t\t<speciality>%s<speciality>\n"
+                            "\t\t<group1>%s</group1>\n"
+                            "\t\t<group2>%s</group2>\n"
+                            "\t</cathedra>\n"
+                            "</teacher>\n\n",
+                            this, fName, lName, pensDate, hours, rating, cName, sName, g1Name, g2Name);
+                        char res[MSG_LENGTH];
+                        sprintf(res, "<teacher>\n"
+                                "\t<id>%i</id>\n"
+                                "\t<firstName>%s</firstName>\n"
+                                "\t<lastName>%s</lastName>\n"
+                                "\t<pensionDate>%s</penstionDate>\n"
+                                "\t<hours>%d</hours>\n"
+                                "\t<rating>%.2f</rating>\n"
+                                "\t<cathedra nameCathedra=\"%s\">\n"
+                                "\t\t<speciality>%s<speciality>\n"
+                                "\t\t<group1>%s</group1>\n"
+                                "\t\t<group2>%s</group2>\n"
+                                "\t</cathedra>\n"
+                                "</teacher>\n\n",
+                               this, fName, lName, pensDate, hours, rating, cName, sName, g1Name, g2Name);
+                        sprintf(result,
+                                "HTTP/1.1 200 OK\n"
+                                "Content-length: %zu\n"
+                                "Content-type: application/xml\n"
+                                "\n"
+                                "%s\0",
+                                strlen(res), res);
+                        socket_write_string(clientSocket, result);
+                        return 1;
+                        }
+                    }
+        else{
+        int id = atoi(strtok(req.uri, "/teachers"));
+        if(list_find_id(list, id) != -1)
+            {
+            if(strcmp(req.method, "GET") == 0)
+                {
+                if(teacher_to_message(list_get(list, list_find_id(list, id)), id) != NULL)
+                    {
+                        char result[BUFFER_LENGTH];
+                        sprintf(result,
+                            "HTTP/1.1 200 OK\n"
+                            "Content-length: %zu\n"
+                            "Content-type: application/xml\n"
+                            "\n"
+                            "%s\0",
+                            strlen(teacher_to_message(list_get(list, list_find_id(list, id)), id)), teacher_to_message(list_get(list, list_find_id(list, id)), id));
+                        socket_write_string(clientSocket, result);
+                        return 1;
+                    }
+                }
+                else if(strcmp(req.method, "DELETE") == 0)
+                {
+                    free_teacher(list_remove(list, list_find_id(list, id)));
+                    char result_msg[MSG_LENGTH];
+                    sprintf(result_msg,
+                            "<message>\n"
+                            "\t<status>ok</status>\n"
+                            "\t<text>Teacher (id%i) has been deleted</text>\n"
+                            "</message>\n", id);
+                    socket_write_string(clientSocket, result_msg);
+                    return 1;
+                }else {
+                    char result_msg[MSG_LENGTH];
+                    sprintf(result_msg,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>NOT ALLOWED</text>\n"
+                            "</message>\n");
+                    socket_write_string(clientSocket, result_msg);
+                    return 1;
+                }
+            }
+            else {
+                char result_msg[MSG_LENGTH];
+                sprintf(result_msg,
+                        "<message>\n"
+                        "\t<status>error</status>\n"
+                        "\t<text>Teacher (id%i) not found</text>\n"
+                        "</message>\n", id);
+                socket_write_string(clientSocket, result_msg);
+                return 1;
+            }
+
+            }
+        }
+    else
+    {
+            char result_msg[MSG_LENGTH];
+            sprintf(result_msg,
+                            "<message>\n"
+                            "\t<status>error</status>\n"
+                            "\t<text>Not Found</text>\n"
+                            "</message>\n");
+            socket_write_string(clientSocket, result_msg);
+            return 1;
+    }
+}
+
---
+++ courses/prog_base_2/labs/lab4/xmlprocess.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
+#include "teahers.h"
+
+void print_teacher(teacher_t *self){
+    printf("\tName:\t\t%10s\n\tSurname:\t%10s\n\tPension:\t%10s\n\tHours:\t%10d\n\tRating:\t%10.2f\n\tCathedra:\n\t\tName:\t%10s\n\t\tSpeciality:%10s\n\t\tGroups:\n\t\t\t%10s\n\t\t\t%10s\n", self->name, self->surname, self->date, self->hours, self->rating, self->cathedra->name, self->cathedra->speciality, self->cathedra->groups[0], self->cathedra->groups[1]);
+}
+
+void xml_out(char *text){
+    xmlDoc * xDoc;
+    xDoc = xmlReadMemory(text, strlen(text), NULL, NULL, 0);
+    if(NULL == xDoc){
+        printf("Error reading file");
+        return;
+    }
+    xmlNode * xRootEl;
+    xRootEl = xmlDocGetRootElement(xDoc);
+    for(xmlNode * xCur = xRootEl->children; NULL != xCur ;xCur = xCur->next){
+        if(XML_ELEMENT_NODE == xCur->type){
+            printf("Tag name <%s>\n", xCur->name);
+            for(xmlNode *xJ = xCur->children; xJ != NULL; xJ = xJ->next){
+                if(XML_ELEMENT_NODE == xJ->type){
+                    if(strcmp(xJ->name, "cathedra") == 0){
+                        xmlNode *xCath = xJ;
+                        char *name = xmlGetProp(xCath, "name");
+                        printf("\t%10s: %s\n", xCath->name, name);
+                        for(xmlNode *xGr = xCath->children->next; xGr != NULL; xGr = xGr->next){
+                            if(XML_ELEMENT_NODE == xGr->type){
+                                //!THE FOLLOWING CODE IS MORE CORRECT, BUT SLOWER AND WITH THE SAME RESULT XD
+                                /*if(!strcmp(xGr->name, "groups")){
+                                    printf("\t%10s:\n", xGr->name);
+                                    for(xmlNode *xAgr = xGr->children->next; xAgr != NULL; xAgr = xAgr->next){
+                                        if(XML_ELEMENT_NODE == xAgr->type){
+                                            const char *content = xmlNodeGetContent(xAgr);
+                                            printf("\t\t%s\n", content);
+                                        }
+                                    }
+                                    continue;
+                                }*/
+                                const char *content = xmlNodeGetContent(xGr);
+                                printf("\t%10s: %s\n", xGr->name, content);
+                            }
+                        }
+                        continue;
+                    }
+                    const char *content = xmlNodeGetContent(xJ);
+                    printf("\t%10s: %s\n", xJ->name, content);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+int teachers_count(char *text){
+    xmlDoc * xDoc;
+    xDoc = xmlReadMemory(text, strlen(text), NULL, NULL, 0);
+    if(NULL == xDoc){
+        printf("Error reading file");
+        return 0;
+    }
+    int count = 0;
+    xmlNode * xRootEl;
+    xRootEl = xmlDocGetRootElement(xDoc);
+    for(xmlNode * xCur = xRootEl->children; NULL != xCur ;xCur = xCur->next){
+        if(XML_ELEMENT_NODE == xCur->type){
+            count++;
+        }
+    }
+    return count;
+}
+
+void xml_extract(char *text, list_t *list){
+    xmlDoc * xDoc;
+    xDoc = xmlReadMemory(text, strlen(text), NULL, NULL, 0);
+    if(NULL == xDoc){
+        printf("Error reading file");
+        return;
+    }
+
+    xmlNode * xRootEl;
+    xRootEl = xmlDocGetRootElement(xDoc);
+    int i = 0;
+    for(xmlNode * xCur = xRootEl->children; NULL != xCur, i < teachers_count(text); xCur = xCur->next){
+        if(XML_ELEMENT_NODE == xCur->type){
+            teacher_t *teachers = new_teacher();
+            //printf("Tag name <%s>\n", xCur->name);
+            for(xmlNode *xJ = xCur->children; xJ != NULL; xJ = xJ->next){
+                if(XML_ELEMENT_NODE == xJ->type){
+                    if(!strcmp(xJ->name, "cathedra")){
+                        xmlNode *xCath = xJ;
+                        char *name = xmlGetProp(xCath, "name");
+                        //printf("\t%10s: %s\n", xCath->name, name);
+                        strcpy(teachers->cathedra->name, name);
+                        for(xmlNode *xGr = xCath->children->next; xGr != NULL; xGr = xGr->next){
+                            if(XML_ELEMENT_NODE == xGr->type){
+                               if(!strcmp(xGr->name, "groups")){
+                                    int j = 0;
+                                    for(xmlNode *xAgr = xGr->children->next; xAgr != NULL; xAgr = xAgr->next){
+                                        if(XML_ELEMENT_NODE == xAgr->type){
+                                            if(!strcmp(xAgr->name, "group")){
+                                                const char *content = xmlNodeGetContent(xAgr);
+                                                teachers->cathedra->groups[j] = content;
+                                                j++;
+                                            }
+                                        }
+                                    }
+                                    continue;
+                                } else if(!strcmp(xGr->name, "speciality")){
+                                    const char *content = xmlNodeGetContent(xGr);
+                                    strcpy(teachers->cathedra->speciality, content);
+                                }
+                            }
+                        }
+                        continue;
+                    }
+                    else {
+                        const char *content = xmlNodeGetContent(xJ);
+                        if(!strcmp(xJ->name, "name")){
+                            strcpy(teachers->name, content);
+                        }
+                        else if(!strcmp(xJ->name, "surname")){
+                            strcpy(teachers->surname, content);
+                        }
+                        else if(!strcmp(xJ->name, "pension")){
+                            strcpy(teachers->date, content);
+                        }
+                        else if(!strcmp(xJ->name, "hours")){
+                            teachers->hours = atoi(content);
+                        }
+                        else if(!strcmp(xJ->name, "rating")){
+                            teachers->rating = atof(content);
+                        }
+                    }
+                }
+            }
+            i++;
+            list_add(list, teachers);
+        }
+    }
+}
+
+char *teacher_to_message(teacher_t *self, int id){
+    char buff[MSG_LENGTH] = {"\0"};
+    sprintf(buff, "<teacher>\n"
+                "\t<id>%i</id>\n"
+                "\t<firstName>%s</firstName>\n"
+                "\t<lastName>%s</lastName>\n"
+                "\t<pensionDate>%s</penstionDate>\n"
+                "\t<hours>%d</hours>\n"
+                "\t<rating>%.2f</rating>\n"
+                "\t<cathedra nameCathedra=\"%s\">\n"
+                "\t\t<speciality>%s<speciality>\n"
+                "\t\t<group1>%s</group1>\n"
+                "\t\t<group2>%s</group2>\n"
+                "\t</cathedra>\n"
+                "</teacher>\n\n",
+                id, self->name, self->surname, self->date, self->hours, self->rating,
+                self->cathedra->name, self->cathedra->speciality, self->cathedra->groups[0], self->cathedra->groups[1]);
+    return buff;
+}
+
+char *all_teachers_to_message(list_t *list){
+    char buff[MSG_LENGTH] = {"\0"};
+    if(list_count(list) == 0){
+        return buff;
+    }
+    sprintf(buff, "%s", teacher_to_message(list_get(list, 0), list_id(list, 0)));
+    for(int i = 1; i  < list_count(list); i++){
+        sprintf(buff, "%s\n%s", buff, teacher_to_message(list_get(list, i), list_id(list, i)));
+    }
+    return buff;
+}
