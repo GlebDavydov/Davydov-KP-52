0805131d27fa7fbf12a896280ce4cc5e6ce63e37
GlebDavydov <davydovkp52@gmail.com>
2016-05-28 22:58:45 +0300

    I've been doing it wrong for too long

--- courses/prog_base_3/battlefield/main.cpp
+++ courses/prog_base_3/battlefield/main.cpp
@@ -5,51 +5,53 @@
 
 #include "main.h"
 
-enum direction{N, NE, E, SE, S, SW, W, NW};
+typedef enum direction{N, NE, E, SE, S, SW, W, NW}direction;
 
 using namespace sf;
 
-class map_point{
-    public:
-        int x;
-        int y;
-        int occupied;
-        Sprite sp;
-        map_point(){
-            x = 0;
-            y = 0;
-            occupied = 1;
-        }
-        map_point(int xc, int yc, int isocc){
-            if(x >= 30 || x < 0 || y >= 30 || y < 0){
-                x = 0;
-                y = 0;
-            }
-            x = xc;
-            y = yc;
-            occupied = isocc;
-            sp.setPosition(307 + 30*x, 8 + 30*y);
-        }
-        void textureSet(Texture txt){
-            sp.setTexture(txt);
-        }
-};
+typedef enum land_type_s{
+    GRSS = 1,
+    WALL
+}land;
 
-/*class battle_robot{
+typedef struct position{
+    int x;
+    int y;
+}position;
+
+class battle_robot{
     public:
         int maxAp;
         int currAp;
-        int dir;
-        map_point position;
-};*/
+        direction dir;
+        position pos;
+};
 
 //int star_distance_count(map bf, map_point start, map_point dest);
 
 int DLL_EXPORT battlefield(RenderWindow& window){
+    land battlefield[15][15]{
+        {WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL},//1
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//2
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//3
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//4
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//5
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//6
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//7
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//8
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//9
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//10
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//11
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//12
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//13
+        {WALL,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,GRSS,WALL},//14
+        {WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL}//15
+    };
     window.clear(Color::Black);
-    Texture land, obst; // allowed, restricted, pers, bn, bne, be, bse, bs, bsw, bw, bnw;
-    land.loadFromFile("land.png");
-    obst.loadFromFile("obst.png");
+    Texture grass, stone, bfbg;
+    bfbg.loadFromFile("bfbg.jpg");
+    grass.loadFromFile("land.png");
+    stone.loadFromFile("obst.png");
     /*allowed.loadFromFile("allowed.png");
     restricted.loadFromFile("restricted.png");
     pers.loadFromFile("personel.png");
@@ -61,27 +63,34 @@ int DLL_EXPORT battlefield(RenderWindow& window){
     bsw.loadFromFile("bot_sw.png");
     bw.loadFromFile("bot_w.png");
     bnw.loadFromFile("bot_nw.png");*/
-    map_point point[30][30];
-    for(int i = 0; i < 30*30; i++){
-        if(i % 43 != 0 && i != 0){
-                point[i/30][i%30] = *(new map_point(i/30, i%30, 0));
-                point[i/30][i%30].textureSet(land);
-        } else {
-                point[i/30][i%30] = *(new map_point(i/30, i%30, 1));
-                point[i/30][i%30].textureSet(obst);
+    Sprite bfback(bfbg);
+    bfback.setPosition(0,0);
+    Sprite bfsprite[15][15];
+    for(int i = 0; i < 15*15; i++){
+        bfsprite[i/15][i%15].setPosition(433+30*(i/15), 133+30*(i%15));
+        switch(battlefield[i/15][i%15]){
+        case WALL:
+            bfsprite[i/15][i%15].setTexture(stone);
+            break;
+        case GRSS:
+        default:
+            bfsprite[i/15][i%15].setTexture(grass);
         }
     }
-    Event event;
-    while(window.pollEvent(event)){
-        for(int i = 0; i < 30*30; i++){
-            window.draw(point[i/30][i%30].sp);
-        }
-        if (event.type == Event::KeyPressed){
-            if (event.key.code == sf::Keyboard::Escape){
-                return 0;
+    while(1){
+        Event event;
+        while(window.pollEvent(event)){
+            window.draw(bfback);
+            for(int i = 0; i < 15*15; i++){
+                window.draw(bfsprite[i/15][i%15]);
             }
+            if (event.type == Event::KeyPressed){
+                if (event.key.code == sf::Keyboard::Escape){
+                    return 0;
+                }
+            }
+            window.display();
         }
-        window.display();
     }
     return 1;
 }
